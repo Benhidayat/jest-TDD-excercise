2fc29be30b3d91fcbcd7a4f83a06917954fd4b17
benhidayat <98013492+Benhidayat@users.noreply.github.com>
2024-10-02 00:16:30 +0700

    build test for reversing starting
    test work when reversing a single word, multiple
    words and also work with numbers and punctuations

--- app.test.js
+++ app.test.js
@@ -1,7 +1,26 @@
 const capitalize = require('./capitalize');
+const reverseString = require('./reverseString');
 
 describe('capitalize string', () => {
     it('should return string with its first letter capitalized', () => {
         expect(capitalize('hello')).toBe('Hello');
+    })
+})
+
+describe('reverse string', () => {
+    it('reverse a single word', () => {
+        expect(reverseString('hello')).toBe('olleh');
+    })
+
+    it('reverse multiple words', () => {
+        expect(reverseString('good morning')).toBe('gninrom doog');
+    })
+
+    it('reverse words with numbers and punctuations', () => {
+        expect(reverseString('abc 123!')).toBe('!321 cba');
+    })
+
+    it('return empty string', () => {
+        expect(reverseString('')).toBe('');
     })
 })
